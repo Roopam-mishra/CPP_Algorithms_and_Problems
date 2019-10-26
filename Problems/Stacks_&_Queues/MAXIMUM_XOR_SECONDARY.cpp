935646cc50a707719bc7cf923db9999f42b483ce
Roopam-mishra <45690038+Roopam-mishra@users.noreply.github.com>
2019-10-27 03:00:25 +0530

    Create MAXIMUM_XOR_SECONDARY.cpp

---
+++ Problems/Stacks_&_Queues/MAXIMUM_XOR_SECONDARY.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long int i,n,ans,a[1000000];
+    stack<long long int> st;
+	cin>>n;
+	for(i=0;i<n;i++)
+	cin>>a[i];
+	ans=0;
+	for(i=0;i<n;i++)
+	{
+	    while(!st.empty())
+	    {
+	        if(st.top()<a[i])
+	        {
+	            ans=max(ans,st.top()^a[i]);
+	            st.pop();
+	        }
+	        else
+	        {
+	            ans=max(ans,st.top()^a[i]);
+	            break;
+	        }
+	    }
+	    st.push(a[i]);
+	}
+	cout<<ans<<endl;
+	return 0;
+}
