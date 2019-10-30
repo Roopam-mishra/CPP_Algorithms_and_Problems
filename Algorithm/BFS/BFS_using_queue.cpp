8e8c247b9b9f5ba469729fab8cfca2bcf013e1b6
Roopam-mishra <45690038+Roopam-mishra@users.noreply.github.com>
2019-10-31 00:00:51 +0530

    Create BFS_using_queue.cpp

---
+++ Algorithm/BFS/BFS_using_queue.cpp
@@ -0,0 +1,47 @@
+/*
+Author: Roopam Mishra
+*/
+**********************
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n,a,b,i,j;
+    cin>>n;
+    vector<ll> adj[n+1];
+    for(i=0;i<n-1;i++)
+    {
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    queue<ll> q;
+    q.push(1);
+    cout<<1<<" ";
+    ll visited[n+1];
+    for(i=0;i<=n;i++)
+    visited[i]=0;
+    ll level[n+1];
+    level[1]=1;
+    visited[1]=1;
+    while(!q.empty())
+    {
+        ll x=q.front();
+        for(i=0;i<adj[x].size();i++)
+        {
+            if(visited[adj[x][i]]==0)
+            {
+                level[adj[x][i]]=level[x]+1;
+                visited[adj[x][i]]=1;
+                q.push(adj[x][i]);
+                cout<<adj[x][i]<<" ";
+            }
+        }
+        q.pop();
+    }
+}
